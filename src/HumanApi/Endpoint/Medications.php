4981c06ec1fef12c28b04a1e2b2fbc099cf63dc5
Dhanesh <dhanesh.narvekar@mydiabeteshome.com>
2018-04-19 13:28:38 +0530

    New endpoints added into humanapi

---
+++ src/HumanApi/Endpoint/Medications.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Choccybiccy\HumanApi\Endpoint;
+
+/**
+ * Class Weight
+ * @package Choccybiccy\HumanApi\Endpoint
+ */
+class Medications extends MedicalEndpoint
+{
+
+    /**
+     * @var string
+     */
+    protected $type = "medical/medications";
+    
+    function readCollection(){
+        
+    }
+}
+
+?>
---
+++ src/HumanApi/Endpoint/TestResults.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Choccybiccy\HumanApi\Endpoint;
+
+/**
+ * Class Weight
+ * @package Choccybiccy\HumanApi\Endpoint
+ */
+class TestResults extends MedicalEndpoint
+{
+
+    /**
+     * @var string
+     */
+    protected $type = "medical/test_results";
+    
+    function readCollection(){
+        
+    }
+}
