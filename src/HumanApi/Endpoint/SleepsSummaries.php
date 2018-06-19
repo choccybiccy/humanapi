ccc5ce093ea9fe8e1b260140ebf294375338b76b
Dhanesh <dhanesh.narvekar@mydiabeteshome.com>
2018-06-19 17:41:46 +0530

    Sleep Summaries endpoint added

---
+++ src/HumanApi/Endpoint/SleepsSummaries.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Choccybiccy\HumanApi\Endpoint;
+
+/**
+ * Class ActivitiesSummaries
+ * @package Choccybiccy\HumanApi\Endpoint
+ */
+class SleepsSummaries extends PeriodicEndpoint
+{
+
+    /**
+     * @var string
+     */
+    protected $type = "sleeps/summaries";
+}
