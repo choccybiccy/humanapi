078f6ce561c83d2b44d4bfebd7604a306b8f8a43
mehulpanchal <mehul.panchal@mydiabeteshome.com>
2020-11-18 16:40:16 +0530

    added new Class for report

---
+++ src/HumanApi/Endpoint/Reports.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Choccybiccy\HumanApi\Endpoint;
+
+/**
+ * Class Reports
+ * @package Choccybiccy\HumanApi\Endpoint
+ */
+class Reports extends PeriodicEndpoint
+{
+
+    /**
+     * @var string
+     */
+    protected $type = "medical/reports";
+}
