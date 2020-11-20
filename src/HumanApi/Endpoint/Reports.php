0a7e39449bc06d820778bb110adbca78cdfe8fa5
mehulpanchal <mehul.panchal@mydiabeteshome.com>
2020-11-20 17:11:09 +0530

    endpoint chagne for the report

--- src/HumanApi/Endpoint/Reports.php
+++ src/HumanApi/Endpoint/Reports.php
@@ -6,11 +6,27 @@ namespace Choccybiccy\HumanApi\Endpoint;
  * Class Reports
  * @package Choccybiccy\HumanApi\Endpoint
  */
-class Reports extends PeriodicEndpoint
+class Reports extends MedicalEndpoint
 {
 
     /**
      * @var string
      */
     protected $type = "medical/reports";
+
+    /**
+     * @var bool
+     */
+    protected $listReturnsArray = true;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function buildSpecificEntryUrl($id)
+    {
+    	return $this->buildUrlParts(array(
+            $this->type,
+            $id,
+        ));
+    }
 }
